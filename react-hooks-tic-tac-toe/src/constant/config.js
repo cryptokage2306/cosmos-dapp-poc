d615a56ceeab7cc11d3b67b55eff3898e69c1aa6
Cryptokage <26vivek06@gmail.com>
2022-05-04 19:02:38 +0530

    feat: add polling interval on config

--- react-hooks-tic-tac-toe/src/constant/config.js
+++ react-hooks-tic-tac-toe/src/constant/config.js
@@ -3,3 +3,5 @@ export const CHAIN_ID = process.env.REACT_APP_CHAIN_ID || "";
 export const NAME = process.env.REACT_APP_NAME || "";
 export const RPC_URL = process.env.REACT_APP_RPC_URL || "";
 export const GAS_PRICE = process.env.REACT_APP_GAS_PRICE || "1acudos";
+export const POLLING_INTERVAL = 5;
+export const TEST_ID = /^[0-9]{6,10}?$/;
